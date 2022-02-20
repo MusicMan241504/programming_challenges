8849440a2a607ed01794d02141206cef9a432cb2
Isaac Lavender <musicman241504@gmail.com>
2022-02-20 21:51:39 +0000

    challenge 5

--- objective4.java
+++ objective4.java
@@ -137,10 +137,22 @@ class objective4 {
 
 
 
+    static void challenge5() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n\nGreatest number challenge\n");
+        System.out.print("Enter number 1: ");
+        int num1 = input.nextInt();
+        System.out.print("Enter number 2: ");
+        int num2 = input.nextInt();
+        System.out.println(Math.max(num1,num2));
+    }
+
+
     public static void main(String[] args) {
         //challenge1();
         //challenge2();
         //challenge3();
-        challenge4();
+        //challenge4();
+        challenge5();
     }
 }
