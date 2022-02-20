eb0626c03fa3bc5568c934268a1d07a003c02fc2
Isaac Lavender <musicman241504@gmail.com>
2022-02-20 15:09:37 +0000

    Objective 1

---
+++ objective1.java
@@ -0,0 +1,32 @@
+class objective1 {
+  public static void main(String[] args) {
+        System.out.println("\n\nVisual dice challenge\n");
+
+        System.out.println("oooooooooooo");
+        System.out.println("o          o");
+        System.out.println("o  #    #  o");
+        System.out.println("o    #     o");
+        System.out.println("o  #    #  o");
+        System.out.println("o          o");
+        System.out.println("oooooooooooo");
+
+
+        System.out.println("\n\nASCII art challenge\n");
+
+        System.out.println("susfsjzfenxkzfedwvdsjfkfsub               ksgs                              rbz                                   fty                              shsyglxfgkf");
+        System.out.println("jdfgjsdgvmdkhukvkskwkwytvhe           drhdgdydejhw                         bdjyi                                 virys                          fjhnjfxjckckfjmj");
+        System.out.println("            jrg                     heey        jucg                      verjghf                               vkfyurc                      kcfghbjg      yjyuvry");
+        System.out.println("            kav                   hdsg            jevh                   brsj bsgg                             keyt jtye                   tcghfhg            vkbphg");
+        System.out.println("            wgh                   vshu                                  sjyf   dvgx                           kigu   kygu                 jtrxif");
+        System.out.println("            cek                     hfjg                               ndks     mbsd                         ftjy     rtyu               hgksfy");
+        System.out.println("            jsb                       hafdf                           ftdp       sbad                       gkik       kurc              lbkgju ");
+        System.out.println("            uaw                          hshahf                      gsph         dgri                     ifsb         zrte             jdfgfv ");
+        System.out.println("            msw                              fjefg                  yjtj           cdsj                   uewv           xyte            ixfvbk ");
+        System.out.println("            jev                                 jefd               dgftjesgsgskgjdiyzdja                 jfhybyewgvtkewrvuvtky           kdvbdz ");
+        System.out.println("            hwc                                   kcso            yttdjdhjzvmjdgdjgfdbhsj               tkvtbyuvybuivfeadrgclid           udfgdi ");
+        System.out.println("            jsf                   jdeh            jwgi           tjdb                 nrei             btry                 ctvo           ivgybdt            shrjen");
+        System.out.println("            bou                     kxhd        gehd            besg                   dheg           mied                   ckrt            ukkvdntk      ifnfdbb");
+        System.out.println("juewfguefyffewuhweyfeyfygef           hslksgddlatc             jdfd                     vrjs         jdge                     xyfg              ifkvjdjvtxdhrjd");
+        System.out.println("ukrektyrewiewrtvrwvtrwvyrwe               lsgf                hgdf                       mtsb       jesh                       rciu                heykrvbse");
+	}
+}
