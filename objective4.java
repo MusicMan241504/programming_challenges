4ab3a462a1758fa28b5fee2c6e31b33c111b5233
Isaac Lavender <musicman241504@gmail.com>
2022-02-20 20:13:54 +0000

    Challenge 3

--- objective4.java
+++ objective4.java
@@ -33,9 +33,28 @@ class objective4 {
     }
 
 
+    static void challenge3() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("\n\nVocational grade challenge\n");
+
+        System.out.print("Enter mark: ");
+        int mark = input.nextInt();
+        if (mark >= 80) {
+            System.out.println("DISTINCTION");
+        }else if (mark >= 60) {
+            System.out.println("MERIT");
+        }else if (mark >= 40) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
+    }
+
+
 
     public static void main(String[] args) {
         //challenge1();
-        challenge2();
+        //challenge2();
+        challenge3();
     }
 }
