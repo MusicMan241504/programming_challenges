44bee990beef8729026ed87ee765e69bec74b93d
Isaac Lavender <musicman241504@gmail.com>
2022-02-22 20:34:39 +0000

    installed json-simple

--- src/objective4/challenge9.java
+++ src/objective4/challenge9.java
@@ -1,10 +1,13 @@
 package objective4;
+import org.json.simple.JSONObject;
 
 public class challenge9 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+   public static void main(String[] args) {
+      JSONObject jsonObject = new JSONObject();
+      String text = "Text with special character /\"\'\b\f\t\r\n.";
+      System.out.println(text);
+      System.out.println("After escaping.");
+      text = jsonObject.escape(text);
+      System.out.println(text);
+   }
 }
