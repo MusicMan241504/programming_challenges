816a2a05a7be11bf8780c2a9a5be11f071e47049
Isaac Lavender <musicman241504@gmail.com>
2022-05-03 21:29:25 +0100

    corrected indents

--- src/averages/Main.java
+++ src/averages/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		Scanner input = new Scanner(System.in);			//create scanner object
 		ArrayList<Double> nums = new ArrayList<Double>();	//create list for storing doubles
 		String line = input.nextLine();					//get input and store as string
-		
+
 		String[] numStr = null;
 		if (line.indexOf(", ") != -1) {					//if contains comma and space
 			numStr = line.split(", ");
@@ -27,8 +27,8 @@ public class Main {
 		for (String val : numStr) {
 			nums.add(Double.parseDouble(val));			//convert each index in list to a double by adding to num list
 		}
-		
-		
+
+
 		double mean_val = mean(nums);
 		double med_val = median(nums);
 		ArrayList<Double> mode_vals = mode(nums);
@@ -37,13 +37,13 @@ public class Main {
 		//there may be multiple modes
 		System.out.print("Mode:");
 		for (double mode_val : mode_vals) {
-		      System.out.print(" " + mode_val);
-		    }
+			System.out.print(" " + mode_val);
+		}
 		input.close();
 
 	}
-	
-	
+
+
 	static double mean(ArrayList<Double> nums) {				//method to get mean
 		double total = 0;
 		for (int i = 0; i < nums.size(); i++) {			//get total
@@ -52,8 +52,8 @@ public class Main {
 		double answer = total / nums.size();			//divide by length of array
 		return answer;
 	}
-	
-	
+
+
 	static double median(ArrayList<Double> nums) {				//method to get median
 		double answer = 0;
 		Collections.sort(nums);								//sort
@@ -66,21 +66,21 @@ public class Main {
 		}
 		return answer;
 	}
-	
-	
+
+
 	static ArrayList<Double> mode(ArrayList<Double> nums) {
 		ArrayList<Double> answers = new ArrayList<Double>();
 		Map<Double,Integer> map = new HashMap<>();		//create hashmap
 		for(double number : nums){						//add data to hash map
-		    if(map.containsKey(number)){
-		        map.put(number,map.get(number)+1);
-		    }else{
-		        map.put(number,1);
-		    }
+			if(map.containsKey(number)){
+				map.put(number,map.get(number)+1);
+			}else{
+				map.put(number,1);
+			}
 		}
-		
+
 		int max = (Collections.max(map.values()));		//get highest value in hashmap
-		
+
 		for(Map.Entry<Double, Integer> vals : map.entrySet()) {	//get key from val
 			if(vals.getValue() == max) {
 				answers.add(vals.getKey());
