cd2ee9522c582713c847f6b13bbc50920207c3e1
Isaac Lavender <musicman241504@gmail.com>
2022-08-25 21:39:10 +0100

    challenge2

--- src/cgp/page20/Challenge2.java
+++ src/cgp/page20/Challenge2.java
@@ -1,9 +1,37 @@
 package cgp.page20;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Challenge2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+
+		char startChar;
+		ArrayList<String> items = new ArrayList<String>();
+
+		for(startChar = 'A'; startChar <= 'Z'; startChar++) {
+			System.out.print("Enter an item starting with '" + startChar + "': ");
+			items.add(input.nextLine());
+
+			System.out.print("I went to the market and bought");
+			for(int i = 0; i < items.size(); i++) {
+				if(items.size() - i == 1) {//if 1 item left
+					System.out.println(" a " + items.get(i));
+				}else if(items.size() - i == 2){//if 2 items left
+					System.out.print(" a " + items.get(i) + " and");
+				}else {
+
+					System.out.print(" a " + items.get(i) + ",");
+				}
+			}
+		}
+
+
+
+
+		input.close();
 
 	}
 
