95d038f06d65f936e6dfef84025b0d01a09175e3
Isaac Lavender <musicman241504@gmail.com>
2022-08-25 21:52:12 +0100

    start challenge3

--- src/cgp/page20/Challenge2.java
+++ src/cgp/page20/Challenge2.java
@@ -6,29 +6,30 @@ import java.util.Scanner;
 public class Challenge2 {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
+		try (Scanner input = new Scanner(System.in)) {
+			char startChar;
+			ArrayList<String> items = new ArrayList<String>();
 
-		char startChar;
-		ArrayList<String> items = new ArrayList<String>();
+			for (startChar = 'A'; startChar <= 'Z'; startChar++) {
+				System.out.print("Enter an item starting with '" + startChar + "': ");
+				String userInput = input.nextLine();
+				items.add(userInput);
 
-		for (startChar = 'A'; startChar <= 'Z'; startChar++) {
-			System.out.print("Enter an item starting with '" + startChar + "': ");
-			items.add(input.nextLine());
+				System.out.print("I went to the market and bought");
+				for (int i = 0; i < items.size(); i++) {
+					if (items.size() - i == 1) {// if 1 item left
+						System.out.println(" a " + items.get(i));
+					} else if (items.size() - i == 2) {// if 2 items left
+						System.out.print(" a " + items.get(i) + " and");
+					} else {
 
-			System.out.print("I went to the market and bought");
-			for (int i = 0; i < items.size(); i++) {
-				if (items.size() - i == 1) {// if 1 item left
-					System.out.println(" a " + items.get(i));
-				} else if (items.size() - i == 2) {// if 2 items left
-					System.out.print(" a " + items.get(i) + " and");
-				} else {
-
-					System.out.print(" a " + items.get(i) + ",");
+						System.out.print(" a " + items.get(i) + ",");
+					}
 				}
 			}
-		}
 
-		input.close();
+			input.close();
+		}
 
 	}
 
---
+++ src/cgp/page20/Challenge3.java
@@ -0,0 +1,10 @@
+package cgp.page20;
+
+public class Challenge3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
