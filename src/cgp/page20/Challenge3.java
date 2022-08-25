0638b1fc318d8c62cb8a2055f1fc4ccba369a906
Isaac Lavender <musicman241504@gmail.com>
2022-08-25 22:11:47 +0100

    challenge3

--- src/cgp/page20/Challenge3.java
+++ src/cgp/page20/Challenge3.java
@@ -1,9 +1,18 @@
 package cgp.page20;
 
+import java.util.Scanner;
+
 public class Challenge3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the snakes name: ");;
+		String name = input.nextLine();
+		
+		System.out.println("                     .-=-.          .--.\n         __        .'     '.       /  \" )	     " + name + "       	\n _     .'  '.     /   .-.   \\     /  .-'\\\n( \\   / .-.  \\   /   /   \\   \\   /  /    ^\n \\ `-` /   \\  `-'   /     \\   `-`  /\n  `-.-`     '.____.'       `.____.'");
+		
+		
+		input.close();
 
 	}
 
