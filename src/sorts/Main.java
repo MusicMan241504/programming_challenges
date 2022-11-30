06d8abe24417b60e269d409935a75aebea1a1384
Isaac Lavender <musicman241504@gmail.com>
2022-11-30 16:55:16 +0000

    Merge sprt completed

--- src/sorts/Main.java
+++ src/sorts/Main.java
@@ -4,15 +4,23 @@ package sorts;
 public class Main {
 
 	public static void main(String[] args) {
-		double[] list = randArray(50000,0,100000);
-		double[] sortedList = insertionSort(list);
-		for(double item : sortedList) {
-			System.out.println(item);
-		}
+		double[] list = randArray(1000000,0,1000000);
+		//printArray(list);
+		System.out.println("\n\n\n");
+		//bubbleSort(list);
+		//insertionSort(list);
+		mergeSort(list,0,list.length-1);
+		printArray(list);
 
 	}
 	
-	public static double[] bubbleSort(double[] list) {
+	public static void printArray(double[] list) {
+		for(double item : list) {
+			System.out.println(item);
+		}
+	}
+	
+	public static void bubbleSort(double[] list) {
 		boolean swaps = true;
 		while(swaps) {
 			swaps = false;
@@ -25,10 +33,9 @@ public class Main {
 				}
 			}
 		}
-		return list;
 	}
 	
-	public static double[] insertionSort(double[] list) {
+	public static void insertionSort(double[] list) {
 		for (int i = 1; i < list.length; i++) {
 			double tmp = list[i];
 			int i2 = i-1;
@@ -39,7 +46,6 @@ public class Main {
 			list[i2+1] = tmp;
 			
 		}
-		return list;
 	}
 	
 	public static double[] randArray(int len, int min, int max) {
@@ -48,6 +54,55 @@ public class Main {
 			list[i] = Math.random()*(max-min)+min;
 		}
 		return list;
+	}
+	
+	public static void mergeSort(double[] list, int left, int right) {
+		if (left < right) {
+			int mid = (left+right)/2;
+			mergeSort(list, left, mid);
+			mergeSort(list, mid+1, right);
+			merge(list, left, mid, right);
+		}
+	}
+	
+	public static void merge(double[] list, int left, int mid, int right) {
+		//find length of both arrays
+		int n1 = mid-left+1;
+		int n2 = right-mid;
+		//create new arrays
+		double array1[] = new double[n1];
+		double array2[] = new double[n2];
+		//add values to arrays
+		for (int i1 = 0; i1 < n1; ++i1) {
+			array1[i1] = list[i1+left];
+		}
+		for (int i2 = 0; i2 < n2; ++i2) {
+			array2[i2] = list[i2+mid+1];
+		}
+		//merge the arrays
+		int i1 = 0, i2 = 0;
+		int i3 = left;
+		while (i1 < n1 && i2 < n2) {
+			if (array1[i1] <= array2[i2]) {
+				list[i3] = array1[i1];
+				i1++;
+			}else {
+				list[i3] = array2[i2];
+				i2++;
+			}
+			i3++;
+		}
+		//deal with different lengthed arrays
+		while (i1 < n1) {
+			list[i3] = array1[i1];
+			i1++;
+			i3++;
+		}
+		while (i2 < n2) {
+			list[i3] = array2[i2];
+			i2++;
+			i3++;
+		}
 	}
 
 }
