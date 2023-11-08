fbbc76ffca41f644f4370b56946623ae674b2521
Isaac Lavender <musicman241504@gmail.com>
2023-11-08 17:20:04 +0000

    renamed method

--- src/recursion/Triangle.java
+++ src/recursion/Triangle.java
@@ -3,15 +3,15 @@ package recursion;
 public class Triangle {
 
 	public static void main(String[] args) {
-		int num = fib(4);
+		int num = tri(4);
 		System.out.print(num);
 	}
 	
-	static int fib(int num) {
+	static int tri(int num) {
 		if (num == 1) {
 			return num;
 		}else {
-			num += fib(num-1);
+			num += tri(num-1);
 			return num;
 		}
 		
